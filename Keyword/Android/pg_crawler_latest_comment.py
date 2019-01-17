1976927832204371f03c83353e8f053aaa9cd7ca
space <3721assistant@gmail.com>
2019-01-17 13:58:31 +0800

    revert

--- Keyword/Android/pg_crawler_latest_comment.py
+++ Keyword/Android/pg_crawler_latest_comment.py
@@ -1,122 +1,107 @@
-#encoding=utf8
-import sys, os
+# encoding=utf8
 
 from pymongo import MongoClient
+import trans
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
 from langdetect import detect
 import time
 import datetime
 import trans
-import nltk
-import re
-import collections
-
 # use http://gearman.org/examples/reverse/ later
-porter_stemmer = nltk.stem.PorterStemmer()
-wordnet_lemmatizer = nltk.stem.WordNetLemmatizer()
-def stem(str):
-    return porter_stemmer.stem(str)
-    # return wordnet_lemmatizer.lemmatize(str)
 
-def match_stemmed(key,str):
-    ks=stem(key.lower())
-    nltk_tokens = nltk.word_tokenize(str.lower())
-    for w in nltk_tokens:
-        if ks in w:
-            return True
-    return False
-def get_next_word(key,str):
-    try:
-        return str.lower().split(key)[1].split()[0]
-    except:
-        return ''
+
+def gen_key_list():
+    keyList = {}
+    with open("./keyword.txt") as f:
+        pkeyList = f.read().splitlines()
+
+    current_category = ""
+    for p in pkeyList:
+        if p:
+            if p.startswith(";"):
+                continue
+            elif p.startswith("#"):
+                current_category = p.split()[1]
+            else:
+                if current_category in keyList.keys():
+                    keyList[current_category].append(p)
+                else:
+                    keyList[current_category] = [p]
+
+    # print(keyList)
+    return keyList
+
+
+def gen_pipe_with_key(keys):
+    pipe = {}
+
+    for key in keys.keys():
+        tmp_pipe = [{"$match": {"$or": []}}]
+
+        # prepare for [or] type query
+        tmp_or = []
+
+        # prepare for [and] type query
+        # tmp_and = []
+
+        for kw in keys[key]:
+            tmp_or.append({"usr_comment": {"$regex": kw}})
+
+        tmp_pipe[0]["$match"]["$or"] = tmp_or
+        pipe[key] = tmp_pipe
+
+
+    # print(pipe)
+    return pipe
+
+
+def convertTimeStamp2Str(timeStamp):
+    tArray = time.localtime(timeStamp)
+    return time.strftime("%Y-%m-%d", tArray)
+
 
 if __name__ == '__main__':
 
+    # generate key word dict
+    keylist = gen_key_list()
+    # generate query pipe
+    p = gen_pipe_with_key(keylist)
 
     client = MongoClient('mongodb://192.168.10.143:27017')
     reivew_db = client['pg_crawler']
 
-# query app id first
-    pipe = [
-            {'$group': {'_id': '$appid', 'count': {'$sum': 1}}},
-            {'$sort': {'count': -1}}
-        ]
+    comments_obj = []
 
-    rs = reivew_db['comment'].aggregate(pipeline=pipe)
-#    appid_aggregate = []
+    for t in p.keys():
+        rs = reivew_db['comment'].aggregate(pipeline=p[t])
 
-# for each appid
-    # ll = 1
-    # keyword_list=['fake','scam','fraud']
-    keyword_list=['dont','do not',"don't"]
-    keyword_list_count={ i : 0 for i in keyword_list }
-    star_list=['1']
-    nextword_list=[]
-    print (datetime.datetime.now())
-    print ('keywords: ',keyword_list,'star: ',star_list)
-    for i in rs:
-        hit=False
-        stars=[]
-#        print i['_id']
-        #print i["_id"]
-        if reivew_db['offline'].find({"appid": i["_id"]}).count() == 0:
-#        if offline) == 0:
-            for ireivew in reivew_db['comment'].find({"appid": i["_id"]}):
-                #print detect(ireivew['usr_name'])
-                review = ireivew['usr_comment']
-                review_title = ireivew['usr_name']
-                review_star = ireivew['usr_star']
-                review_date = trans.trans(ireivew['language'],ireivew['usr_date'])
-                stars.append(review_star)
-                # print (ireivew['_id'],ireivew['language'],ireivew['usr_date'],review_date,trans.timestamp2date(review_date))
-                #if review.lower().find(u"廣告") != -1 or review_title.lower().find(u"廣告") != -1:
-                #if review.lower().find("dumb") != -1 or review.lower().find("dumb") != -1:
-                #if review.lower().find("trash") != -1 or review.lower().find("trash") != -1:
-                #if review.lower().find(u"只有广告") != -1 or review_title.lower().find(u"只有广告") != -1:
-                # if review.lower().find(u"广告") != -1 or review_title.lower().find(u"广告") != -1:
-                #if review.lower().find(u"审核") != -1 or review_title.lower().find(u"审核") != -1:
-                #if review.lower().find(u"骗人") != -1 or review_title.lower().find(u"骗人") != -1:
-                #if review.lower().find(u"骗子") != -1 or review_title.lower().find(u"骗子") != -1:
-                #if review.lower().find(u"骗钱") != -1 or review_title.lower().find(u"骗钱") != -1:
-                #if review.lower().find(u"欺诈") != -1 or review_title.lower().find(u"欺诈") != -1:
-                #if review.lower().find(u"自启") != -1 or review_title.lower().find(u"自启") != -1:
-                # if review.lower().find(u"病毒") != -1 or review_title.lower().find(u"病毒") != -1:
-                # if review.lower().find(u"话费") != -1 or review_title.lower().find(u"话费") != -1:
-                #if review.lower().find(u"不要下载") != -1 or review_title.lower().find(u"不要下载") != -1:
-                #if review.lower().find(u"不要安装") != -1 or review_title.lower().find(u"不要安装") != -1:
-                #if review.lower().find(u"假的") != -1 or review_title.lower().find(u"假的") != -1:
-                # if review.lower().find("fake") != -1 or review_title.lower().find("fake") != -1:
-                #if review.lower().find("can't play") != -1 or review_title.lower().find("can't play") != -1:
-                # if review.lower().find("can't use") != -1 or review_title.lower().find("can't use") != -1:
-                # if review.lower().find("scam") != -1 or review_title.lower().find("scam") != -1:
-                # if review.lower().find("fraud") != -1 or review_title.lower().find("fraud") != -1:
-                #if review.lower().find("cheat") != -1 or review_title.lower().find("cheat") != -1:
-                #if review.lower().find("ads") != -1 or review_title.lower().find("ads") != -1:
-                # if review.lower().find("virus") != -1 or review_title.lower().find("virus") != -1:
-                # if review.lower().find("torjan") != -1 or review_title.lower().find("torjan") != -1:
-                # if review.lower().find("root") != -1 or review_title.lower().find("root") != -1:
+        for i in rs:
+            i["type"] = t
+            comments_obj.append(i)
 
-                if review_star in star_list:
-                    for j in keyword_list:
+    cluster = {}
+    for obj in comments_obj:
+        if obj["appid"] in cluster.keys():
+            cluster[obj["appid"]].append(obj)
+        else:
+            cluster[obj["appid"]] = [obj]
 
-                        if review.lower().find(j) != -1:
 
-                        # if match_stemmed(j,review):
-                            keyword_list_count[j]+=1
-                        # ll += 1
-                        # if ll % 20 == 0:
-                        #     t = raw_input()
-                        # print ireivew
-                            hit=True
-                            print ("\t %s %s %s %s %s: %s" %(i["_id"], ireivew['usr_star'], j, trans.timestamp2date(review_date), ireivew['usr_name'], ireivew['usr_comment']))
-                            nextword_list.append ((get_next_word(j,review)))
-        if hit:
-            print (i["_id"],':',len(stars))
-            # print ()
-            for j in range(1,6):
-                print (j,':',stars.count(str(j)),end = " ",sep="")
-            print ()
-    client.close()
-    print (keyword_list_count)
-    print (collections.Counter(nextword_list))
-    print ("**** done *****")
+    ll = 0
+    for app in cluster.keys():
+        print(">> Clustering: {}, Got {} matching reviews.".format(app, len(cluster[app])))
+        for review in cluster[app]:
+            date = "nil"
+            try:
+                date = convertTimeStamp2Str(trans.trans(review['language'], review['usr_date']))
+            except:
+                date = "nil"
+            ll += 1
+            if ll % 20 == 0:
+                t = raw_input()                
+            print("\t[{}] [{}] {} {}".format(date, review["type"], review["usr_star"], review["usr_comment"]))
+
